c3e66e7427d4fb589030c54039e4522823fafb43
KarlAlonne <87619289+KarlAlonne@users.noreply.github.com>
2022-06-04 19:08:54 -0300

    Create BMexercicio08.java

---
+++ BMexercicio08.java
@@ -0,0 +1,14 @@
+import java.
+
+public class exerciseFive {
+public static void main(String[] args){
+
+    for(int i = 1; i < 100; i++){  
+        if (i % 3 == 0) { 
+            System.out.println(i); 
+        }
+    }
+
+}
+}
+© 2022 GitHub, Inc.
